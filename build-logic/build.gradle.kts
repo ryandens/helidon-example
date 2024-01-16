6eb37a203b5a7f6077a277304faaf8afec27bdc9
Ryan Dens <ryandens735@gmail.com>
2024-01-16 00:01:06 -0800

    :heavy_plus_sign: add spotless to build

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,6 +7,14 @@
 plugins {
     // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
     `kotlin-dsl`
+    alias(libs.plugins.spotless)
+}
+
+spotless {
+    kotlinGradle {
+        target("*.gradle.kts", "src/main/kotlin/*.gradle.kts")
+        ktlint()
+    }
 }
 
 repositories {

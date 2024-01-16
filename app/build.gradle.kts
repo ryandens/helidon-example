91952705a9bae3725eae0694724a03ba2913a751
Ryan Dens <ryandens735@gmail.com>
2024-01-16 11:17:39 -0800

    :fire: remove unused libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,3 @@
 plugins {
     id("com.ryandens.example.helidon-application-conventions")
 }
-
-dependencies {
-    implementation("org.apache.commons:commons-text")
-    implementation(project(":utilities"))
-}

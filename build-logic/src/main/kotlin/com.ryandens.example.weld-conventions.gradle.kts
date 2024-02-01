26210830cbdd3fe2dfa20a8b4bf7d76d2403ee97
Ryan Dens <ryandens735@gmail.com>
2024-02-01 15:49:01 -0800

    :bulb: document weld issue so i don't have to read all those issues ever again

--- build-logic/src/main/kotlin/com.ryandens.example.weld-conventions.gradle.kts
+++ build-logic/src/main/kotlin/com.ryandens.example.weld-conventions.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 }
 
 // not a jib specific issue, but described well here: https://github.com/GoogleContainerTools/jib/issues/1488
+// We mark the beans directory as part of the source set so that it renders properly as part of the IDE experience
 sourceSets {
     named("main") {
         java {
@@ -16,6 +17,11 @@ sourceSets {
     }
 }
 
+
+// this allows us to treat the beans file specially and removes the need for the workarounds described in the
+// jib issue and described here https://github.com/helidon-io/helidon/blob/4.0.3/examples/quickstarts/helidon-quickstart-mp/build.gradle#L97
+// in a way that lets helidon tests and jib have the beans.xml file in a location that is compatible with weld
+// ideally, weld would look in any classpath entry for the beans.xml but that is far outside the scope of this repository
 tasks.register<Copy>("processBeansXml") {
     from(layout.projectDirectory.file("src/main/beans/beans.xml"))
     into(layout.buildDirectory.dir("classes/java/main/META-INF"))

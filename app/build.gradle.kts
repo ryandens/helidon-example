1f4b3abd5725dfed096063cdad8750187328c973
Ryan Dens <ryandens735@gmail.com>
2025-07-13 14:11:45 -0700

    :heavy_plus_sign: mockito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,3 +2,9 @@ plugins {
     id("com.ryandens.example.helidon-application-conventions")
     id("com.ryandens.example.helidon-jib-conventions")
 }
+
+dependencies {
+    testImplementation(platform("org.mockito:mockito-bom:5.18.0"))
+    testImplementation("io.helidon.microprofile.testing:helidon-microprofile-testing-mocking")
+    testImplementation("org.mockito:mockito-core")
+}

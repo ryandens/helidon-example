50c6c99cea8de1bcdb7f149ce03adb7fb862d862
Ryan Dens <ryandens735@gmail.com>
2025-07-13 15:48:08 -0700

    :sparkles: avoid mockito inline mocker

---
+++ build-logic/src/main/kotlin/com/ryandens/example/JavaagentVersionUtil.kt
@@ -0,0 +1,29 @@
+package com.ryandens.example
+
+import org.gradle.api.Action
+import org.gradle.api.NamedDomainObjectProvider
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.MutableVersionConstraint
+import org.gradle.api.artifacts.component.ModuleComponentIdentifier
+
+object JavaagentVersionUtil {
+
+    fun fromDependencyAndConfiguration(
+        dependency: Dependency,
+        configuration: NamedDomainObjectProvider<Configuration>,
+    ): Action<in MutableVersionConstraint> =
+        Action<MutableVersionConstraint> {
+            val artifact =
+                configuration
+                    .get()
+                    .incoming
+                    .artifactView {
+                        componentFilter {
+                            it.displayName.startsWith("${dependency.group}:${dependency.name}")
+                        }
+                    }.artifacts
+                    .single()
+            strictly((artifact.id.componentIdentifier as ModuleComponentIdentifier).version)
+        }
+}

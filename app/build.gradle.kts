810a8c4cf9f6b0cd6503f0c0257421eda3fda609
Ryan Dens <ryandens735@gmail.com>
2025-07-13 15:48:08 -0700

    :sparkles: avoid mockito inline mocker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,16 @@
+import com.ryandens.example.JavaagentVersionUtil
+
 plugins {
     id("com.ryandens.example.helidon-application-conventions")
     id("com.ryandens.example.helidon-jib-conventions")
+    id("com.ryandens.javaagent-test") version "0.8.0"
 }
 
 dependencies {
     testImplementation(platform("org.mockito:mockito-bom:5.18.0"))
     testImplementation("io.helidon.microprofile.testing:helidon-microprofile-testing-mocking")
     testImplementation("org.mockito:mockito-core")
+    testJavaagent("org.mockito:mockito-core") {
+        version(JavaagentVersionUtil.fromDependencyAndConfiguration(this, configurations.testRuntimeClasspath))
+    }
 }

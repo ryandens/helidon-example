a7da7fcca3b7f1ef9466a3467874ca12ef44beef
Ryan Dens <ryandens735@gmail.com>
2025-07-13 16:10:55 -0700

    :recycle: minimal extraction to convention plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,10 @@ import com.ryandens.example.JavaagentVersionUtil
 plugins {
     id("com.ryandens.example.helidon-application-conventions")
     id("com.ryandens.example.helidon-jib-conventions")
-    id("com.ryandens.javaagent-test") version "0.8.0"
+    id("com.ryandens.example.mockito-conventions")
 }
 
 dependencies {
-    testImplementation(platform("org.mockito:mockito-bom:5.18.0"))
     testImplementation("io.helidon.microprofile.testing:helidon-microprofile-testing-mocking")
     testImplementation("org.mockito:mockito-core")
     testJavaagent("org.mockito:mockito-core") {
---
+++ build-logic/src/main/kotlin/com.ryandens.example.mockito-conventions.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    id("com.ryandens.example.java-common-conventions")
+    id("com.ryandens.javaagent-test")
+}

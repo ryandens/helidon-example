62d9ceeaf1f192c86969a3456ab79d1e2889ca6e
Ryan Dens <ryandens735@gmail.com>
2025-07-13 16:44:53 -0700

    :recycle: use resolutionStrategy

--- build-logic/src/main/kotlin/com.ryandens.example.mockito-conventions.gradle.kts
+++ build-logic/src/main/kotlin/com.ryandens.example.mockito-conventions.gradle.kts
@@ -1,4 +1,16 @@
+import com.ryandens.example.JavaagentVersionUtil
+
 plugins {
     id("com.ryandens.example.java-common-conventions")
     id("com.ryandens.javaagent-test")
+}
+
+configurations.testJavaagent {
+    resolutionStrategy {
+        eachDependency {
+            if (requested.version.isNullOrBlank()) {
+                useVersion(JavaagentVersionUtil.versionFromDependencyAndConfiguration(requested, configurations.testRuntimeClasspath))
+            }
+        }
+    }
 }

fcd4029a9e00f825f3fdc44460d2b1f58b0cfc56
Ryan Dens <ryandens735@gmail.com>
2025-07-13 18:26:35 -0700

    :fire: upgrade plugin to reduce boilerplate

--- build-logic/src/main/kotlin/com.ryandens.example.mockito-conventions.gradle.kts
+++ build-logic/src/main/kotlin/com.ryandens.example.mockito-conventions.gradle.kts
@@ -1,16 +1,4 @@
-import com.ryandens.example.JavaagentVersionUtil
-
 plugins {
     id("com.ryandens.example.java-common-conventions")
     id("com.ryandens.javaagent-test")
-}
-
-configurations.testJavaagent {
-    resolutionStrategy {
-        eachDependency {
-            if (requested.version.isNullOrBlank()) {
-                useVersion(JavaagentVersionUtil.versionFromDependencyAndConfiguration(requested, configurations.testRuntimeClasspath))
-            }
-        }
-    }
 }

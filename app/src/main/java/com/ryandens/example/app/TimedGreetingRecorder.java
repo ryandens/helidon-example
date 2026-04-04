ffc59af044f019a019807d1954e32be0b89e419e
Ryan Dens <ryandens735@gmail.com>
2026-04-04 01:00:49 -0700

    :sparkles: use AutoDelegate to make TimedGreetingRecorder

---
+++ app/src/main/java/com/ryandens/example/app/TimedGreetingRecorder.java
@@ -0,0 +1,21 @@
+package com.ryandens.example.app;
+
+import com.ryandens.delegation.AutoDelegate;
+
+@AutoDelegate(GreetingRecorder.class)
+public class TimedGreetingRecorder extends AutoDelegate_TimedGreetingRecorder
+    implements GreetingRecorder {
+
+  public TimedGreetingRecorder(final GreetingRecorder recorder) {
+    super(recorder);
+  }
+
+  @Override
+  public boolean record() {
+    final long start = System.nanoTime();
+    final var result = super.record();
+    final long end = System.nanoTime();
+    System.out.println("TimedGreetingRecorder: " + (end - start));
+    return result;
+  }
+}

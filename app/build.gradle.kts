789bbb941a40405e8c2a941ac268c7fdf445d45f
Ryan Dens <ryandens735@gmail.com>
2026-04-04 01:00:01 -0700

    :heavy_plus_sign: auto delegate deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 }
 
 dependencies {
+    val autoDelegateVersion = "0.3.1"
+    compileOnly("com.ryandens:auto-delegate-annotations:$autoDelegateVersion")
+    annotationProcessor("com.ryandens:auto-delegate-processor:$autoDelegateVersion")
     testImplementation("io.helidon.microprofile.testing:helidon-microprofile-testing-mocking")
     testImplementation("org.mockito:mockito-core")
     testJavaagent("org.mockito:mockito-core")

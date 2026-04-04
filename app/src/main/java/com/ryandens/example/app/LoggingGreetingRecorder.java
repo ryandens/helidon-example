f94ebeef0c97bc8968eefdf8e3975e0a3b59db3b
Ryan Dens <ryandens735@gmail.com>
2026-04-04 01:01:04 -0700

    :sparkles: register TimedGreetingRecorder

--- app/src/main/java/com/ryandens/example/app/LoggingGreetingRecorder.java
+++ app/src/main/java/com/ryandens/example/app/LoggingGreetingRecorder.java
@@ -1,8 +1,10 @@
 package com.ryandens.example.app;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Typed;
 
 @ApplicationScoped
+@Typed(LoggingGreetingRecorder.class)
 class LoggingGreetingRecorder implements GreetingRecorder {
 
   @Override
